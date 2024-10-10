94b3978d6c1049c990e24d7492a091e7216cdf06
rohitsahu <rohitsahu33531@gmail.com>
2024-10-10 21:23:45 +0530

    updated file

--- Queues/queue_using_list_structure.cpp
+++ Queues/queue_using_list_structure.cpp
@@ -1,66 +1,95 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-struct Node{
+struct Node
+{
     int data;
     Node *next;
 
-}*front=NULL,*rear=NULL;
-int size=0;
+} *front = NULL, *rear = NULL;
+int size = 0;
 
-void enqueue(int val){
-    if(rear==NULL){
-        Node *new_node=(Node*)malloc(sizeof(Node));
-        new_node->data=val;
-        new_node->next=NULL;
-        front=rear=new_node;
+void enqueue(int val)
+{
+    Node *new_node = (Node *)malloc(sizeof(Node));
+    if (new_node == NULL)
+    {
+        cout << "queue overflow" << endl;
+        return;
+    }
+    new_node->data = val;
+    new_node->next = NULL;
+    if (rear == NULL)
+    {
+
+        front = rear = new_node;
         size++;
         return;
-
     }
-    Node *new_node=(Node*)malloc(sizeof(Node));
-    new_node->data=val;
-    new_node->next=NULL;
-    
-    rear->next=new_node;
-    rear= new_node; //rear=rear->next; 
+
+    rear->next = new_node;
+    rear = new_node; // rear=rear->next;
     size++;
-
-
 }
 
-void dequeue(){
-    if(front==NULL){
-        cout<<"queue is empty"<<endl;
+void dequeue()
+{
+    if (front == NULL)
+    {
+        cout << "queue underflow" << endl;
         return;
     }
-    Node *temp=front;
-    front=front->next;
-    if(front==NULL) rear=NULL;
+    Node *temp = front;
+    front = front->next;
+    if (front == NULL)
+        rear = NULL;
     free(temp);
     size--;
-
 }
 
-void display(){
-    Node *temp=front;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
-        temp=temp->next;
+void display()
+{
+    if (front == NULL)
+    {
+        cout << "queue is empty" << endl;
+        return;
     }
-    cout<<endl;
-
+    Node *temp = front;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
 }
 
-int main(){
-    
- enqueue(10);
- enqueue(20);
- enqueue(30);
- enqueue(40);
- display();
+int main()
+{
+    int choice;
+    while (1)
+    {
+        cout << "enter 1 for enqueue\nenter 2 for dequeue\nenter 3 for display\nenter 4 for exit" << endl;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            cout << "enter data you want to enque" << endl;
+            int data;
+            cin >> data;
+            enqueue(data);
+            break;
+        case 2:
+            dequeue();
+            break;
+        case 3:
+            display();
+            break;
+        case 4:
+            exit(0);
 
- dequeue();
- 
- display();
+        default:
+            break;
+        }
+    }
+
     return 0;
 }
